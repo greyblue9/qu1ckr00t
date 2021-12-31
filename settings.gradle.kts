d69f9e49d46d8bc0948b2f6fa2a964e9c9e26b80
David Reilly <greyblue9@gmail.com>
2021-12-31 07:42:38 +0000

    Rework build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,22 @@
 rootProject.name = "Qu1ckr00t"
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "qu1ckr00t"
+include(
+    ":shared",
+    ":app",
+)
+
+val serviceRoot = "service"
+project(":interface").projectDir = file("$serviceRoot${File.separator}interface")
+project(":service").projectDir = file("$serviceRoot${File.separator}service")
+
+buildCache { local { removeUnusedEntriesAfterDays = 1 } }
 
 include(":shared", ":app")
